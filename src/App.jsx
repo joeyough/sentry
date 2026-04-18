1032ac26c071eeb30bc2201654712c51eb829b56
joeyough <joey@calmplimentz.com>
2026-04-18 17:04:32 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SentryV2 from './sentry-v2'
 import SentryClient from './sentry-client'
+import KnowTheRoom from './know-the-room'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<SentryV2 />} />
         <Route path="/client" element={<SentryClient />} />
+        <Route path="/room" element={<KnowTheRoom />} />
       </Routes>
     </BrowserRouter>
   )
