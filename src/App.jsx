b321bd9d9f7f3bfa8a1659e01f4510598bd60a85
joeyough <joey@calmplimentz.com>
2026-05-08 10:52:38 -0600

    Update App.jsx
    adding dashboard sandbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SentryV2 from './sentry-v2'
 import SentryClient from './sentry-client'
-
+import SentryDashboard from './sentry-dashboard'
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SentryV2 />} />
         <Route path="/client" element={<SentryClient />} />
+        <Route path="/dashboard" element={<SentryDashboard />} />
       </Routes>
     </BrowserRouter>
   )
