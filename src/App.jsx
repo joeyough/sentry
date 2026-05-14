2075077462ffb6547f3fa1532a621982b5d70be0
joeyough <joey@calmplimentz.com>
2026-05-14 15:28:25 -0600

    Update App.jsx
    adding sentry-home and sentry-insights tiered dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SentryHome from './sentry-home'
 import SentryV2 from './sentry-v2'
 import SentryClient from './sentry-client'
 import SentryDashboard from './sentry-dashboard'
+import SentryInsightsTiered from './sentry-insights-tiered'
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SentryV2 />} />
+        <Route path="/home" element={<SentryHome />} />
         <Route path="/client" element={<SentryClient />} />
         <Route path="/dashboard" element={<SentryDashboard />} />
+        <Route path="/insights-tiered" element={<SentryInsightsTiered />} />
       </Routes>
     </BrowserRouter>
   )
